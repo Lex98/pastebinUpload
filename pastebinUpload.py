d4267302df570282578aec2a30d416cc9baeb00d
Lex98 <Lexis098@gmail.com>
2016-12-02 10:51:32 +0300

    add documentation

--- pastebinUpload.py
+++ pastebinUpload.py
@@ -3,12 +3,14 @@ import argparse
 
 
 def reading_text(input_file):
+    """return input file as a string"""
     with open(input_file, 'r') as text_file:
         data = text_file.read()
     return data
 
 
 def parse_argv():
+    """return comand line arguments"""
     parser = argparse.ArgumentParser(description="Upload file to pastebin")
     parser.add_argument("input_file", type=str,
                         help="file to upload")
@@ -16,9 +18,9 @@ def parse_argv():
                         help="format of input text file to syntax highlights. default = python",
                         default="python")
     parser.add_argument("user_name", type=str, nargs='?',
-                        help="user name", default="Lex98")
+                        help="user name", default="")
     parser.add_argument("password", type=str, nargs='?',
-                        help="password", default="46884688")
+                        help="password", default="")
     parser.add_argument("paste_name", type=str, nargs='?',
                         help="name or title of your paste", default="")
     parser.add_argument("paste_private", type=int, nargs='?',
@@ -35,15 +37,33 @@ def upload_to_pastebin(input_text, data_format,
                        user_name, password,
                        paste_name, private,
                        expire_date):
+    """Upload text to pastebin
+
+    Keyword arguments:
+    input_text - text to upload
+    data_format - format of input text file to syntax highlights. see it on http://pastebin.com/api
+    user_name - username
+    password - user password
+    paste_name -  name or title of your paste on pastebin
+    private - private of data. 0 = public 1 = unlisted 2 = private
+    expire_date - expire date. N=Never, 10M=10 Minutes, 1H=1 Hour, 1D=1 Day, 1W=1 Week, 2W=2 Weeks, 1M=1 Month
+
+    print server responce
+    """
     url = "http://pastebin.com/api/api_post.php"
     login_url = "http://pastebin.com/api/api_login.php"
     dev_key = ""
-    login_payload = {"api_dev_key": dev_key,
-                     "api_user_name": user_name,
-                     "api_user_password": password
-                     }
-    login_request = requests.post(login_url, login_payload)
-    user_key = login_request.text
+
+    if user_name == '' and password == '':  # generating user key
+        login_payload = {"api_dev_key": dev_key,
+                         "api_user_name": user_name,
+                         "api_user_password": password
+                         }
+        login_request = requests.post(login_url, login_payload)
+        user_key = login_request.text
+    else:
+        user_key = ""
+
     payload = {"api_option": "paste",
                "api_user_key": user_key,
                "api_paste_private": private,
@@ -58,6 +78,7 @@ def upload_to_pastebin(input_text, data_format,
 
 
 def main():
+    """main function"""
     args = parse_argv()
     input_text = reading_text(args.input_file)
     upload_to_pastebin(input_text, args.paste_format, args.user_name,
